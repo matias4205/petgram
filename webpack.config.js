54ef2ad76203f2193a9f30ed619394bee205c27e
matias4205 <matiasperezpc@hotmail.com>
2020-09-20 18:37:05 -0300

    PWA compatible

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require('path')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
 
 module.exports = {
   output: {
@@ -8,6 +11,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de fotos de mascotas',
+      short_name: 'Petgram 🐶',
+      description: 'Con Petgram puedes encontrar fotos de animales',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ],
   module: {
